c5238fa905de5063b6de86f217a765731b30c99c
mostafa-mahmoud-fathy <162756625+mostafa-mahmoud-fathy@users.noreply.github.com>
2024-12-13 02:06:05 +0200

    Update main.cpp
    name of pyramid tic tac toe has been corrected but the word tic tac toe is not implemented using templates but, all classes inherit from type dedicated parent class

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "5x5_Tic_Tac_Toe.h"
 #include "Misere_TicTacToe.h"
 #include "Ultimate_Tic_Tac_Toe.h"
+#include "Pyramid Tic-Tac-Toe.h"
+#include "Word Tic-Tac-Toe.h"
 
 using namespace std;
 
@@ -11,8 +13,10 @@ void showMenu() {
     cout << "1. Play 4x4 Tic Tac Toe\n";
     cout << "2. Play 5x5 Tic Tac Toe\n";
     cout << "3. Play Misere Tic Tac Toe\n";
-    cout << "4. Play Ultimate Tic Tac Toe\n";  // Added Ultimate option
-    cout << "5. Exit\n";
+    cout << "4. Play Ultimate Tic Tac Toe\n";
+    cout << "5. Play Pyramid Tic Tac Toe\n";
+    cout << "6. Play Word Tic Tac Toe\n";
+    cout << "7. Exit\n";
     cout << "Your choice: ";
 }
 
@@ -23,7 +27,7 @@ int main() {
         showMenu();
         cin >> choice;
 
-        if (choice == 5) {  // Adjusted exit option to 5
+        if (choice == 7) {
             cout << "Thank you for playing!\n";
             break;
         }
@@ -32,117 +36,132 @@ int main() {
         Board<char>* board;
         Player<char>* players[2];
 
+        cout << "Select game mode:\n";
+        cout << "1. Player vs Player\n";
+        cout << "2. Player vs Computer\n";
+        cout << "Your choice: ";
+        int mode;
+        cin >> mode;
+
+        if (mode != 1 && mode != 2) {
+            cout << "Invalid game mode, returning to main menu.\n";
+            continue;
+        }
+
         switch (choice) {
             case 1: { // 4x4 Game
                 cout << "4x4 Tic Tac Toe selected.\n";
-
-                // Create the board
                 board = new TicTacToe_4X4<char>();
 
-                // Player 1 setup
-                cout << "Player 1 (X): Enter name or type 'Random' for a computer player: ";
-                string name1;
-                cin >> name1;
-                if (name1 == "Random") {
-                    players[0] = new Random_Player4X4<char>('X');
-                } else {
-                    players[0] = new Player4X4<char>(name1, 'X');
+                // Player setup
+                for (int i = 0; i < 2; ++i) {
+                    cout << "Player " << (i + 1) << (i == 0 ? " (X): " : " (O): ");
+                    if (mode == 2 && i == 1) {
+                        cout << "Computer player will be used.\n";
+                        players[i] = new Random_Player4X4<char>('O');
+                    } else {
+                        cout << "Enter name or type 'Random' for a computer player: ";
+                        string name;
+                        cin >> name;
+                            players[i] = new Player4X4<char>(name, i == 0 ? 'X' : 'O');
+                    }
                 }
-
-                // Player 2 setup
-                cout << "Player 2 (O): Enter name or type 'Random' for a computer player: ";
-                string name2;
-                cin >> name2;
-                if (name2 == "Random") {
-                    players[1] = new Random_Player4X4<char>('O');
-                } else {
-                    players[1] = new Player4X4<char>(name2, 'O');
-                }
-
                 break;
             }
             case 2: { // 5x5 Game
                 cout << "5x5 Tic Tac Toe selected.\n";
-
-                // Create the board
                 board = new TicTacToe_5X5<char>();
 
-                // Player 1 setup
-                cout << "Player 1 (X): Enter name or type 'Random' for a computer player: ";
-                string name1;
-                cin >> name1;
-                if (name1 == "Random") {
-                    players[0] = new Random_Player5x5<char>('X');
-                } else {
-                    players[0] = new Player5x5<char>(name1, 'X');
+                // Player setup
+                for (int i = 0; i < 2; ++i) {
+                    cout << "Player " << (i + 1) << (i == 0 ? " (X): " : " (O): ");
+                    if (mode == 2 && i == 1) {
+                        cout << "Computer player will be used.\n";
+                        players[i] = new Random_Player5x5<char>('O');
+                    } else {
+                        cout << "Enter name or type 'Random' for a computer player: ";
+                        string name;
+                        cin >> name;
+                            players[i] = new Player5x5<char>(name, i == 0 ? 'X' : 'O');
+                    }
                 }
-
-                // Player 2 setup
-                cout << "Player 2 (O): Enter name or type 'Random' for a computer player: ";
-                string name2;
-                cin >> name2;
-                if (name2 == "Random") {
-                    players[1] = new Random_Player5x5<char>('O');
-                } else {
-                    players[1] = new Player5x5<char>(name2, 'O');
-                }
-
                 break;
             }
             case 3: { // Misere Game
                 cout << "Misere Tic Tac Toe selected.\n";
-
-                // Create the board
                 board = new Misere_Board<char>();
 
-                // Player 1 setup
-                cout << "Player 1 (X): Enter name or type 'Random' for a computer player: ";
-                string name1;
-                cin >> name1;
-                if (name1 == "Random") {
-                    players[0] = new Misere_Random_Player<char>('X');
-                } else {
-                    players[0] = new Misere_Player<char>(name1, 'X');
+                // Player setup
+                for (int i = 0; i < 2; ++i) {
+                    cout << "Player " << (i + 1) << (i == 0 ? " (X): " : " (O): ");
+                    if (mode == 2 && i == 1) {
+                        cout << "Computer player will be used.\n";
+                        players[i] = new Misere_Random_Player<char>( 'O');
+                    } else {
+                        cout << "Enter name or type 'Random' for a computer player: ";
+                        string name;
+                        cin >> name;
+                            players[i] = new Misere_Player<char>(name, i == 0 ? 'X' : 'O');
+                    }
                 }
-
-                // Player 2 setup
-                cout << "Player 2 (O): Enter name or type 'Random' for a computer player: ";
-                string name2;
-                cin >> name2;
-                if (name2 == "Random") {
-                    players[1] = new Misere_Random_Player<char>('O');
-                } else {
-                    players[1] = new Misere_Player<char>(name2, 'O');
-                }
-
                 break;
             }
             case 4: { // Ultimate Game
                 cout << "Ultimate Tic Tac Toe selected.\n";
-
-                // Create the board
                 board = new UltimateBoard<char>();
 
-                // Player 1 setup
-                cout << "Player 1 (X): Enter name or type 'Random' for a computer player: ";
-                string name1;
-                cin >> name1;
-                if (name1 == "Random") {
-                    players[0] = new Ultimate_Random_Player<char>('X');
-                } else {
-                    players[0] = new Ultimate_Player<char>(name1, 'X');
-                }
+                // Player setup
+                for (int i = 0; i < 2; ++i) {
+                    cout << "Player " << (i + 1) << (i == 0 ? " (X): " : " (O): ");
+                    if (mode == 2 && i == 1) {
+                        cout << "Computer player will be used.\n";
+                        players[i] = new Ultimate_Random_Player<char>('O');
+                    } else {
+                        cout << "Enter name or type 'Random' for a computer player: ";
+                        string name;
+                        cin >> name;
+                            players[i] = new Ultimate_Player<char>(name, i == 0 ? 'X' : 'O');
 
-                // Player 2 setup
-                cout << "Player 2 (O): Enter name or type 'Random' for a computer player: ";
-                string name2;
-                cin >> name2;
-                if (name2 == "Random") {
-                    players[1] = new Ultimate_Random_Player<char>('O');
-                } else {
-                    players[1] = new Ultimate_Player<char>(name2, 'O');
+                    }
                 }
+                break;
+            }
+            case 5: { // Pyramid Game
+                cout << "Pyramid Tic Tac Toe selected.\n";
+                board = new PyramidBoard<char>();
 
+                // Player setup
+                for (int i = 0; i < 2; ++i) {
+                    cout << "Player " << (i + 1) << (i == 0 ? " (X): " : " (O): ");
+                    if (mode == 2 && i == 1) {
+                        cout << "Computer player will be used.\n";
+                        players[i] = new RandomPyramidPlayer<char>('O');
+                    } else {
+                        cout << "Enter name or type 'Random' for a computer player: ";
+                        string name;
+                        cin >> name;
+                            players[i] = new PyramidPlayer<char>(name, i == 0 ? 'X' : 'O');
+                    }
+                }
+                break;
+            }
+            case 6:{
+                cout << "Word Tic Tac Toe selected.\n";
+                board = new WordBoard();
+
+                // Player setup
+                for (int i = 0; i < 2; ++i) {
+                    cout << "Player " << (i + 1) << (i == 0 ? " (X): " : " (O): ");
+                    if (mode == 2 && i == 1) {
+                        cout << "Computer player will be used.\n";
+                        players[i] = new WordRandomPlayer('O');
+                    } else {
+                        cout << "Enter name or type 'Random' for a computer player: ";
+                        string name;
+                        cin >> name;
+                        players[i] = new WordPlayer(name, i == 0 ? 'X' : 'O');
+                    }
+                }
                 break;
             }
             default:
